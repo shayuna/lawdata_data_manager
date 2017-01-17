f74994566cd78dde0ada083fd971aa687ff53824
shayuna <shaiz2206@gmail.com>
2017-01-17 09:35:59 +0200

    big changes

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express=require("express");
 var app=express();
 var getData=require("./getData");
+var setData=require("./setData");
 var issueError=require("./issueError");
+var uploadData=require("./uploadData");
 
+app.set("views",__dirname);
+app.set("view engine","jade");
+app.use(express.static(__dirname));
 app.use("/get",getData);
+app.use("/set",setData);
+app.use("/upload",uploadData);
 app.use("/*",issueError);
+
 var PORT = process.env.PORT || 8080;
 app.listen(PORT);
